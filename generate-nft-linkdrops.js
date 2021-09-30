cc2b838608769bb918d0b8521ebd3b48ad8cc44f
Vladimir Grichina <vgrichina@gmail.com>
2021-09-30 16:18:39 -0700

    Generate NFT linkdrops script

---
+++ generate-nft-linkdrops.js
@@ -0,0 +1,40 @@
+const { connect, KeyPair, utils } = require('near-api-js');
+const { InMemoryKeyStore, UnencryptedFileSystemKeyStore } = require('near-api-js/lib/key_stores');
+const { KeyPairEd25519 } = require('near-api-js/lib/utils');
+const { generateSeedPhrase, parseSeedPhrase  } = require('near-seed-phrase');
+
+const LINKDROP_CONTRACT = 'linkdrop.vg';
+
+(async function() {
+    const config = require('./src/config')(process.env.NODE_ENV || 'development');
+    const near = await connect({
+        ...config,
+        keyStore: new UnencryptedFileSystemKeyStore(`${process.env.HOME}/.near-credentials/`)
+    });
+    const { seedPhrase } = generateSeedPhrase(); 
+    console.log('seedPhrase', seedPhrase);
+
+    const account = await near.account(LINKDROP_CONTRACT);
+    for (let i = 0; i < 5; i++) {
+        let { secretKey } = parseSeedPhrase(seedPhrase, `m/44'/397'/0'/${i}'`);
+        let keyPair = KeyPair.fromString(secretKey);
+        console.log(`${config.walletUrl}/linkdrop/${LINKDROP_CONTRACT}/${keyPair}`);
+        await account.functionCall(LINKDROP_CONTRACT, 'send_with_transactions', {
+            public_key: keyPair.getPublicKey().toString().replace('ed25519:', ''),
+            tokens: utils.format.parseNearAmount('0.42'),
+            transactions: [{
+                receiver_id: config.contractName,
+                actions: [{
+                    method_name: "nft_mint_to",
+                    args: JSON.stringify({
+                        receiver_id: "%%RECEIVER_ID%%", 
+                        title: "September NFT Hangout"
+                    }),
+                    gas: "20000000000000",
+                    deposit: "1"
+                }]
+            }]
+        });
+        await account.addKey(keyPair.getPublicKey(), LINKDROP_CONTRACT, ['claim', 'create_account_and_claim'], utils.format.parseNearAmount('1.0'));
+    }
+})();
--- src/config.js
+++ src/config.js
@@ -18,7 +18,7 @@ function getConfig(env) {
     return {
       networkId: 'testnet',
       nodeUrl: 'https://rpc.testnet.near.org',
-      contractName: CONTRACT_NAME || 'humanguild-nft.testnet',
+      contractName: CONTRACT_NAME || 'humanguild-nft.vg',
       walletUrl: 'https://wallet.testnet.near.org',
       helperUrl: 'https://helper.testnet.near.org',
       explorerUrl: 'https://explorer.testnet.near.org',
